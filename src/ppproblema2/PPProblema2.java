13b0368f66e43d4ccb43cd9a22b410069b78b737
karen <karen@KarenZ>
2016-10-11 09:53:35 -0500

    Menu Java

--- src/ppproblema2/PPProblema2.java
+++ src/ppproblema2/PPProblema2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ppproblema2;
-
+import java.util.*;
 /**
  *
  * @author karen
@@ -15,7 +15,31 @@ public class PPProblema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int m;
+       double y,mi,p,Resultados;
+       y=.9144401829;
+       mi=1609.34;
+       p=0.3048;
+       m= solicitarMetros();
     }
+    public static int solicitarMetros(){
+        int m;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce metros");
+        m= entrada.nextInt();
+        return m;
+    }
+    public static void op(){
+        double operaciones;
+        System.out.println("Metros a yardas");
+        operaciones = m * y; 
+    } 
+    public static void RES(){
     
+    
+    
+    }
+
+
+   
 }
